9bce9bc32019fc25fa8780904ccfe5f20391692c
Suraj <suraj.dubey@sunfox.in>
2022-12-02 10:30:36 +0530

    Added jacoco

--- app/src/main/java/com/example/githubaction/MainViewModel.kt
+++ app/src/main/java/com/example/githubaction/MainViewModel.kt
@@ -2,10 +2,10 @@ package com.example.githubaction
 
 import android.app.Application
 import androidx.lifecycle.ViewModel
-import com.example.jacoco.MainState
 import com.saurabhbadola.statesman.BaseViewModel
 
-class MainViewModel(application: Application) :BaseViewModel<MainState>(application) {
+
+class MainViewModel(application: Application) : BaseViewModel<MainState>(application) {
 
 
     override fun createInitialState(): MainState {
