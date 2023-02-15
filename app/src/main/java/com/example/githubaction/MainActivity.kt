a3a488dd8615be349effc3ec4f77556d7a17a40b
Suraj <suraj.dubey@sunfox.in>
2023-02-15 14:35:20 +0530

    File name change

--- app/src/main/java/com/example/githubaction/MainActivity.kt
+++ app/src/main/java/com/example/githubaction/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val s1: Circle = Circle("Red", 2.2)
+        val s2: Rectangle = Rectangle("Yellow", 2.0, 4.0)
+       val result=  s2.res1(3,5)
+        System.out.println(result.toString())
+        System.out.println(s1.toString())
+        System.out.println(s2.toString())
+        val arr= arrayOf(-1,0,3,4)
+        println(arr.filter { a-> a>1 })
+
+
+
     }
 }
